05dd3d0a091d5be31ad51a900ed5f68879a65f97
8946688-cloud <8946688@gmail.com>
2026-05-14 20:18:00 +0800

    Update DayNightSwitch.h

--- DayNightSwitch.h
+++ DayNightSwitch.h
@@ -1,1 +1,15 @@
+#import <UIKit/UIKit.h>
 
+@interface DayNightSwitch : UIView
+
+@property (nonatomic, copy, nullable) void (^changeAction)(BOOL);
+@property (nonatomic) BOOL on;
+@property (nonatomic, nonnull, retain) CAShapeLayer *offBorder;
+@property (nonatomic, nonnull, retain) CAShapeLayer *onBorder;
+@property (nonatomic, nullable, retain) NSArray<UIView *> *stars;
+@property (nonatomic, nonnull, retain) UIImageView *cloud;
+
+- (CGFloat)knobMargin;
+- (void)setOnWithoutAction:(BOOL)on;
+
+@end
