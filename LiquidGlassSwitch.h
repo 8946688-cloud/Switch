b26fbb9a47dec288ffe82bfb5e26a01669d77d41
8946688-cloud <8946688@gmail.com>
2026-05-14 20:18:53 +0800

    Update LiquidGlassSwitch.h

--- LiquidGlassSwitch.h
+++ LiquidGlassSwitch.h
@@ -1,1 +1,7 @@
+#import <UIKit/UIKit.h>
 
+@interface LiquidGlassSwitch : UIView
+@property (nonatomic, copy, nullable) void (^changeAction)(BOOL);
+@property (nonatomic) BOOL on;
+- (void)setOnWithoutAction:(BOOL)on;
+@end
